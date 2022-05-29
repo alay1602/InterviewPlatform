c7563cdc3f68974ce726d7e6d12303a4b4228c51
Smitkumar Rathod <rathorvl97@gmail.com>
2022-05-29 15:33:20 +0530

    backend updated by smit

---
+++ backend/index.js
@@ -0,0 +1,36 @@
+const express=require("express");
+const app=express();
+const server = require("http").Server(app);
+
+const io=require("socket.io")(server ,{
+	cors: {
+		origin: "http://localhost:3000",
+		methods: [ "GET", "POST" ]
+	}
+});
+
+
+io.sockets.on("connection",socket=>{
+   socket.emit("me",socket.id);
+   socket.on("disconnect",()=>{
+       socket.broadcast.emit("callended");
+   })
+
+   socket.on("calluser",(data)=>{
+       io.to(data.userTocall).emit("callUser",{signal:data.signalData,from:data.from,name:data.name});
+
+
+   })
+
+
+   socket.on("answercall",(data)=>{
+       io.to(data.to).emit("callAc",data.signal);
+   })
+
+
+})
+
+server.listen(3000,()=>{
+    console.log("server started");
+})
+
